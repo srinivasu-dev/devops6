f273ba1b5eef7f7c2c0c78497928cf9a4502b007
root <root@ip-172-31-85-25.ec2.internal>
2023-05-11 11:34:22 +0000

    testtest

---
+++ src/main/java/com/marolix/HelloWorld.java
@@ -0,0 +1,29 @@
+package com.marolix;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.Response;
+
+@Path("/hello")
+public class HelloWorld {
+
+    @GET
+    @Path("/echo/{input}")
+    @Produces("text/plain")
+    public String ping(@PathParam("input") String input) {
+        return input;
+    }
+
+    @POST
+    @Produces("application/json")
+    @Consumes("application/json")
+    @Path("/jsonBean")
+    public Response modifyJson(JsonBean input) {
+        input.setVal2(input.getVal1());
+        return Response.ok().entity(input).build();
+    }
+}
+
